3081bf7ce8976fce362e4d195bfbf1ec005718bf
brunogjh <bruno.goh.2020@scis.smu.edu.sg>
2023-10-24 00:56:44 +0800

    latest for aws config

--- db.php
+++ db.php
@@ -1,12 +1,18 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "onlineshop";
+$dbhost = $_SERVER['RDS_HOSTNAME'];
+$dbport = $_SERVER['RDS_PORT'];
+$dbname = $_SERVER['RDS_DB_NAME'];
+$charset = 'utf8' ;
 
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+$username = $_SERVER['RDS_USERNAME'];
+$password = $_SERVER['RDS_PASSWORD'];
+
+$pdo = new PDO($dsn, $username, $password);
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+
+$con = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
 // Check connection
 if (!$con) {
