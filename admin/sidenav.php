7d18db3fb2a3d0cc128050b6f33cc1a657fa5400
rhys <rhystan3@gmail.com>
2023-11-01 23:26:35 +0800

    try2

--- admin/sidenav.php
+++ admin/sidenav.php
@@ -1,13 +1,13 @@
 <?php
 
-  if (!isset($_SESSION['admin_name'])) {
-    $_SESSION['msg'] = "You must log in first";
-    header('location: .././login.php');
-  }
+//   if (!isset($_SESSION['admin_name'])) {
+//     $_SESSION['msg'] = "You must log in first";
+//     header('location: .././login.php');
+//   }
   if (isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['admin_name']);
-    header("location: .././login.php");
+    header("location: ./login.php");
   }
 ?>
 <!DOCTYPE html>
--- server/server.php
+++ server/server.php
@@ -82,7 +82,7 @@ if (isset($_POST['login_admin'])) {
 
   if (count($errors) == 0) {
     $password = $password;
-    $query = "SELECT * FROM admin_info WHERE admin_name='$admin_username' AND admin_password='$password'";
+    $query = "SELECT * FROM admin_info WHERE admin_name='admin' AND admin_password='adminpassword'";
     $results = mysqli_query($db, $query);
     if (mysqli_num_rows($results) == 1) {
        $_SESSION['admin_email'] = $email;
