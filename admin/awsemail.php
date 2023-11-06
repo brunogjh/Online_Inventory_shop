24b8f58fb9281faa9ecec7510950e910aa2d22b4
domteow <dominicteow@gmail.com>
2023-11-06 21:12:18 +0800

    Update awsemail.php

--- admin/awsemail.php
+++ admin/awsemail.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     var_dump($_POST);
     $newStatus = $_POST['orderStatus'];
     $orderId = $_POST['orderId'];
-
+// 
     // Now you can work with $newStatus and $orderId as needed.
 
     echo "$newStatus";
