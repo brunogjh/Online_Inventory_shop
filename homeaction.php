2889b5370c18dbb393a5180fb53956efaff57904
rhys <rhystan3@gmail.com>
2023-11-07 11:41:24 +0800

    l

--- homeaction.php
+++ homeaction.php
@@ -12,7 +12,7 @@ if(isset($_POST["categoryhome"])){
 					<!-- NAV -->
 					<ul class='main-nav nav navbar-nav'>
                     <li class='active'><a href='index.php'>Home</a></li>
-					<li style='padding-left: 3%'><a href='store.php'>All</a></li>
+					<li style='padding-right: 3%'><a href='store.php'>All</a></li>
 					
 	";
 	$category_query = "SELECT * FROM categories WHERE cat_id!=1";
