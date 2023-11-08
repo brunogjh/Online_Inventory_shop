4449f42173ee4f1a32899b1672de5fb7fdcac726
rhys <rhystan3@gmail.com>
2023-11-08 19:25:42 +0800

    f

--- action.php
+++ action.php
@@ -271,7 +271,7 @@ if (isset($_POST["count_item"])) {
 		$sql = "SELECT COUNT(*) AS count_item FROM cart WHERE user_id = $_SESSION[uid]";
 	}else{
 		//When user is not logged in then we will count number of item in cart by using users unique ip address
-		// $sql = "SELECT COUNT(*) AS count_item FROM cart WHERE ip_add = '$ip_add' AND user_id < 0";
+		$sql = "SELECT COUNT(*) AS count_item FROM cart WHERE ip_add = '$ip_add' AND user_id < 0";
 	}
 	
 	$query = mysqli_query($con,$sql);
